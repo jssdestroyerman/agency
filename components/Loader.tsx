c2863015ad62fdd000b7a3f7321d22531c730118
destroyerman <jesuisdestroyerman@proton.me>
2023-02-13 21:31:21 +0100

    working

--- components/Loader.tsx
+++ components/Loader.tsx
@@ -1,53 +1,58 @@
 import { motion } from "framer-motion";
+import { useState } from "react";
 
 const container = {
-    show: { transition: { staggerChildren: 0.5 } },
+    hidden: { transition: { delay: 4 } },
+    show: { transition: { staggerChildren: 0.3 } },
 };
 
 const item = {
-    show: { height: 0, transition: { duration: 1 } },
+    show: { y: "100%", transition: { duration: 1 } },
 };
 
 const item2 = {
-    show: { width: 0, transition: { duration: 0.5 } },
+    show: { x: "100%", opacity: 0, transition: { duration: 1 } },
 };
 
 function Loader() {
+    const [animation, setAnimation] = useState(true);
+
     return (
         <motion.div
-            className="absolute flex flex-col"
+            className={`absolute flex flex-col ${!animation && "hidden"}`}
             variants={container}
             initial="hidden"
             animate="show"
+            onAnimationComplete={() => setAnimation(false)}
         >
-            <div className="h-[50vh] flex rotate-180 flex-row-reverse">
+            <div className="h-[50vh] flex ">
+                <motion.div
+                    className="bg-black h-full w-[100vw] md:w-[33.333vw]"
+                    variants={item}
+                />
                 <motion.div
                     className="bg-black h-full w-[33.333vw]"
                     variants={item}
-                ></motion.div>
-
+                />
                 <motion.div
                     className="bg-black h-full w-[33.333vw]"
                     variants={item}
-                ></motion.div>
-                <motion.div
-                    className="bg-black h-full w-[33.333vw]"
-                    variants={item}
-                ></motion.div>
+                />
             </div>
-            <div className="h-[50vh] flex z-20 flex-row-reverse">
+
+            <div className="h-[50vh] flex z-20 overflow-hidden">
                 <motion.div
-                    className="bg-black h-full w-[33.333vw]"
+                    className="bg-black h-full w-[33.333vw] overflow-hidden"
                     variants={item2}
-                ></motion.div>
+                />
                 <motion.div
-                    className="bg-black h-full w-[33.333vw]"
+                    className="bg-black h-full w-[33.333vw] overflow-hidden"
                     variants={item2}
-                ></motion.div>
+                />
                 <motion.div
-                    className="bg-black h-full w-[33.333vw]"
+                    className="bg-black h-full w-[33.333vw] overflow-hidden"
                     variants={item2}
-                ></motion.div>
+                />
             </div>
         </motion.div>
     );
